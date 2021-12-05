cb16e20b639071b80d2fde8ac22fad2e0d04bc5e
rayana Omar <rayanaomar_@hotmail.com>
2021-12-05 20:57:19 +0300

    PART1

--- ColdCallP1/ViewController.swift
+++ ColdCallP1/ViewController.swift
@@ -9,8 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let names: [String] = ["rayana", "Deem", "Sama", "Yazan","Aleen"]
+    
+    
+    @IBAction func ColdCallButton(_ sender: UIButton) {
+        nameLabel.text = names.randomElement()!
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameLabel.text = "Ready?"
         // Do any additional setup after loading the view.
     }
 
